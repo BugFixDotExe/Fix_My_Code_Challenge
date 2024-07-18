93c645967465a8f9ab533982b124c902814e7bbf
bugfix.exe <bugfix.exe@gmail.com>
2024-07-18 09:31:30 +0100

    solution to square.py, added documentation and made the class attributes into instance attributes

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This script stirves to become a perfect Square calulator """
+
+
+class Square():
+    """The Square class"""
+    def __init__(self, *args, **kwargs):
+        """The constructor of the Square class
+        Args:
+            *args (a list of values)
+            **kwargs (a keyworded dict)
+        """
+        self.width = 0
+        self.height = 0
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        """ Perimiter of the square """
+        return 4 * (self.width)
+
+    def __str__(self):
+        """ A string representation of the square """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    """ A runner for the script as long as it's not improted"""
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
